01df176cb2c41a28d58c71e53805c9e363b69848
unknown <voldam@ lolamgit config --global user.email volidam>
2024-05-02 19:30:15 +0300

    it was homework

---
+++ main.js
@@ -0,0 +1,7 @@
+let text = document.getElementById('red');
+let text_background = prompt("Enter body backraundColor")
+let text_color = prompt("Enter text color")
+
+
+text.style.backgroundColor = text_background
+text.style.color = text_color
